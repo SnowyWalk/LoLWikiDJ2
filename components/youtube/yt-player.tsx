f823a268841962ded1c206e6e00188ed8ebb6967
SnowyWalk <chunsa1166jp@gmail.com>
2026-07-27 11:07:52 +0900

    iframe YT API로 설치하는 과정 만드는중

--- components/youtube/yt-player.tsx
+++ components/youtube/yt-player.tsx
@@ -1,24 +1,35 @@
 "use client";
 
 import {cn} from "@/lib/utils.ts";
+import {useEffect, useRef} from "react";
 
 type YtPlayerProps = {
     className?: string;
 }
 
 export default function YtPlayer({className}: YtPlayerProps) {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const playerRef = useRef<YT.Player | null>(null);
 
-
+    useEffect(() => {
+        
+        const createPlayer = () => {
+            if (playerRef.current || !containerRef.current)
+                return;
+            
+            playerRef.current = new YT.Player(containerRef.current, {
+                height: "100%",
+                width: "100%",
+                videoId: "dQw4w9WgXcQ",
+            })
+        }
+        
+        
+    }, []);
+    
     return (
         <div className={cn("relative w-full aspect-video", className)}>
-            <iframe
-                className="absolute inset-0 w-full h-full"
-                src="https://www.youtube.com/embed/XO8uwiM0PdA"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-            ></iframe>
+            <div ref={containerRef} className="absolute inset-0 w-full h-full"/>
         </div>
     )
 }
